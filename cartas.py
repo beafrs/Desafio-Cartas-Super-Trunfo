e14ff6871049f3b83c599f4bcceb1423bdfcc112
beafrs <beatrizvr946@gmail.com>
2025-09-25 16:29:31 -0400

    Create cartas.py

---
+++ cartas.py
@@ -0,0 +1,6 @@
+baralho = [
+    {"nome": "Carro A", "velocidade": 180, "potência": 120, "peso": 1000},
+    {"nome": "Carro B", "velocidade": 200, "potência": 150, "peso": 950},
+    {"nome": "Carro C", "velocidade": 170, "potência": 110, "peso": 1100},
+    {"nome": "Carro D", "velocidade": 220, "potência": 160, "peso": 900},
+]
